acf81f16929008ae9b5fefffd0c458bd53e453c3
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-05-13 17:37:29 -0500

    Added a server that has a api

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const express = require("express");
 
 const server = express();
 
+server.get('/hello', (req, res) => {
+    res.status(200).json({ api: {hello: "Web 27"}})
+})
 const PORT = 5000;
 server.listen(PORT, () => console.log(`\n ** API on http://localhost:${PORT} **\n`));
