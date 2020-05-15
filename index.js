bbb1bdb3fa05bcd101b068a18d974642b40fbd6f
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-05-14 19:24:00 -0500

    Sending data into my server

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-const express = require("express");
+const express = require("express");//npm i express
+const shortid = require('shortid');
 
-const server = express();
+const server = express(); // npm i shortid
+
+let hubs = [];
+
+server.use(express.json()); // add this line
 
 server.get('/hello', (req, res) => {
-    res.status(200).json({ api: {hello: "Web 27"}})
-})
+    
+    res.status(200).json({ api: {
+    "hello": "Web 27",
+    }})
+});
+
+server.post("/api/hubs", (req,res) => {
+    const hubInfo = req.body;
+    // you validate the data from the user that they sent 
+    hubInfo.id = shortid.generate();
+    hubs.push(hubInfo);
+    res.status(201).json(hubInfo);
+});
+
+//creater lessons
+
+// write a GET /hllo endpoint that returns an 
+// object like so: { hello: "Web 27" } 👇👇👇👇👊 
+
 const PORT = 5000;
 server.listen(PORT, () => console.log(`\n ** API on http://localhost:${PORT} **\n`));
